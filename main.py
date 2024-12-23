eac2deaf75525f00723c77c2a35fb877127dedb3
Dawn <t.diepenbroek@live.com>
2024-12-23 18:59:42 +0200

    adding gitignore

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+player = Player
 
 game_is_on = True
 while game_is_on:
--- player.py
+++ player.py
@@ -1,7 +1,16 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-
+"""
+Create a turtle player that starts at the bottom of the screen and listen for the "Up"
+ keypress to move the turtle north. If you get stuck, check the video walkthrough in 
+ Step 3.
+"""
 class Player:
-    pass
+    def __init__(self):
+        turtle = Turtle
+        turtle.shape = "turtle"
+        turtle.setposition = STARTING_POSITION
